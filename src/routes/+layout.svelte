84bd17481bee54d97c232bf1454eb02440a3d565
mabel <bigbelk@live.ru>
2023-11-19 09:32:43 +0300

    readme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import '@yababay67/sveltekit-components/styles/dummy.scss'
+    //import '@yababay67/sveltekit-components/styles/publications.scss'
+    //import '@yababay67/sveltekit-components/styles/with-navbar.scss'
+    //import '@yababay67/sveltekit-components/styles/without-footer.scss'
+    import { Layout } from '@yababay67/sveltekit-components'
+    import type { SeoProps } from '@yababay67/sveltekit-components/types'
+    import { author, brand, title, subtitle, description, keywords, license, url, menu } from '../app.json'
+
+    const seo: SeoProps = { author, brand, title, subtitle, description, keywords, license, url }
+</script>
+
+<Layout {seo}>
+    <slot />
+</Layout>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<article>
+<h1>Lorem ipsum dolor sit amet</h1> 
+
+<p>
+...consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet purus gravida quis blandit turpis cursus in. Egestas pretium aenean pharetra magna ac. Egestas dui id ornare arcu odio ut sem nulla pharetra. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Malesuada fames ac turpis egestas. Ipsum a arcu cursus vitae congue mauris rhoncus aenean vel. Sed arcu non odio euismod. Lobortis feugiat vivamus at augue eget arcu dictum varius. Ultricies integer quis auctor elit sed vulputate. Justo eget magna fermentum iaculis eu non. Est velit egestas dui id ornare arcu odio ut. Ullamcorper malesuada proin libero nunc consequat.
+</p>
+
+<p>
+Nibh venenatis cras sed felis eget. Elementum nibh tellus molestie nunc non blandit. Aliquam vestibulum morbi blandit cursus risus. Vel pretium lectus quam id. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Egestas integer eget aliquet nibh praesent tristique. Tincidunt eget nullam non nisi est sit amet facilisis magna. Eget mauris pharetra et ultrices neque ornare aenean euismod. Non odio euismod lacinia at quis risus sed. Netus et malesuada fames ac turpis egestas integer. Volutpat sed cras ornare arcu.
+</p>
+</article>
