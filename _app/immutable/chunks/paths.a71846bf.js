6f1b4bfc9bbb839caf83abe42ccd0f17eadc42a8
mabel <bigbelk@live.ru>
2023-11-20 09:48:03 +0300

    Updates

---
+++ _app/immutable/chunks/paths.a71846bf.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1497dv8)==null?void 0:s.base)??"/sveltekit-templates";var e;const a=((e=globalThis.__sveltekit_1497dv8)==null?void 0:e.assets)??t;export{a,t as b};
