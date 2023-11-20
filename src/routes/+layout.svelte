a439ac85c10a7f1409513e3f7b717b854e14b7be
mabel <bigbelk@live.ru>
2023-11-20 06:31:06 +0300

    no-env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
     //import '@yababay67/sveltekit-components/styles/publications.scss'
     //import '@yababay67/sveltekit-components/styles/with-navbar.scss'
     //import '@yababay67/sveltekit-components/styles/without-footer.scss'
+    import './+layout.scss'
     import { Layout } from '@yababay67/sveltekit-components'
     import type { SeoProps, BootstrapSource } from '@yababay67/sveltekit-components/types'
-    import { author, brand, title, subtitle, description, keywords, license, url, menu } from '../app.json'
-    import { PUBLIC_WITH_BOOTSTRAP } from '$env/static/public'
+    import { author, brand, title, subtitle, description, keywords, license, url, bootstrap as BOOTSTRAP_SOURCE } from '../app.json'
 
-    const bootstrap = PUBLIC_WITH_BOOTSTRAP as BootstrapSource
+    const bootstrap = BOOTSTRAP_SOURCE as BootstrapSource
     const seo: SeoProps = { author, brand, title, subtitle, description, keywords, license, url }
 </script>
 
