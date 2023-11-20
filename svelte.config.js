d43dd0ca1a1dfd422b488670c0a757f47ff0a6cf
mabel <bigbelk@live.ru>
2023-11-20 11:40:00 +0300

    gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,21 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+        // Uncomment next lines for gh-pages deploy.
+        /*
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/<your-project-on-github>' : '',
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
+        */
 	}
 };
 
 export default config;
+
