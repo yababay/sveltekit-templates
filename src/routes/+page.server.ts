6fab7ecda6d268b0f898e7f14b88812cc10bbe31
mabel <bigbelk@live.ru>
2023-11-20 10:19:00 +0300

    name-from-package

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,7 @@
 import { getTableOfContent } from "@yababay67/sveltekit-components/markdown"
 import { name as repo } from "../../package.json"
-import { base } from '$app/paths'
 
 export async function load({ fetch }){
-    let [  html ] = await getTableOfContent(fetch, repo)
-    html = html.replace(/href=\"\/articles/g, `href=\"${base}/articles`)
+    const [  html ] = await getTableOfContent(fetch, repo)
     return { html }
 }
