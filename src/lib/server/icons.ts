fb2c54d0277b3f243c667fa4d749d7a0db7c6733
mabel <bigbelk@live.ru>
2023-11-20 07:06:00 +0300

    no-lib

--- src/lib/server/icons.ts
+++
@@ -1,6 +0,0 @@
-import { readFile } from 'fs/promises'
-
-export async function getIcon (name: string) {
-    const path = `./node_modules/bootstrap-icons/icons/${name}.svg`
-    return await readFile(path, 'utf8')
-}
