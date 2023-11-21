66362ac0f33f4248322ae2eeee8c96ea73bd3314
mabel <bigbelk@live.ru>
2023-11-21 12:29:00 +0300

    lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { name } from '../../package.json'
+import { title, description } from '../app.json'
+
+export { name, title, description }
