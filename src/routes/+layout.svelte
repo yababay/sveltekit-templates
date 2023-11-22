3e4565836c15ef7768187e395379680aa6734d69
mabel <bigbelk@live.ru>
2023-11-22 04:07:15 +0300

    layout.scss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     //import '@yababay67/sveltekit-components/styles/publications.scss'
     //import '@yababay67/sveltekit-components/styles/with-navbar.scss'
     //import '@yababay67/sveltekit-components/styles/without-footer.scss'
+    import './+layout.scss'
     import { Layout } from '@yababay67/sveltekit-components'
     import type { SeoProps, BootstrapSource } from '@yababay67/sveltekit-components/types'
     import { author, brand, title, subtitle, description, keywords, license, url, menu } from '../app.json'
