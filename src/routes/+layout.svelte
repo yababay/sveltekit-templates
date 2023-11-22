3de6d840f46074a80b349976ff99a5301332c2ee
mabel <bigbelk@live.ru>
2023-11-23 02:30:35 +0300

    laf

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import '@yababay67/sveltekit-components/styles/publications.scss'
+    //import '@yababay67/sveltekit-components/styles/dummy.scss'
+    //import '@yababay67/sveltekit-components/styles/with-navbar.scss'
+    //import '@yababay67/sveltekit-components/styles/without-footer.scss'
+    import './+layout.scss'
+    import { Layout } from '@yababay67/sveltekit-components'
+    import type { SeoProps, BootstrapSource } from '@yababay67/sveltekit-components/types'
+    import { author, title, subtitle, description, keywords, license, url, bootstrap as BOOTSTRAP_SOURCE } from '../app.json'
+
+    const bootstrap = BOOTSTRAP_SOURCE as BootstrapSource
+    const seo: SeoProps = { author, title, subtitle, description, keywords, license, url }
+</script>
+
+<Layout {seo} {bootstrap} >
+    <slot />
+</Layout>
